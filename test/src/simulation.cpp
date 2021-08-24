e3205074f17760032155a15c403b7f2e48a54ec9
computations <bwbettisworth@gmail.com>
2021-08-24 16:54:10 +0200

    Reduce test runtime

--- test/src/simulation.cpp
+++ test/src/simulation.cpp
@@ -21,13 +21,13 @@ TEST_CASE("Simulation, basics", "[simulation]") {
     CHECK(r[3] == Approx(0.25).margin(0.1));
   }
   SECTION("Eval, smaller margins") {
-    auto r   = t.eval(1000000);
+    auto r   = t.eval(100000);
     auto sum = std::accumulate(r.begin(), r.end(), 0.0);
     CHECK(sum == Approx(1.0));
-    CHECK(r[0] == Approx(0.25).margin(0.001));
-    CHECK(r[1] == Approx(0.25).margin(0.001));
-    CHECK(r[2] == Approx(0.25).margin(0.001));
-    CHECK(r[3] == Approx(0.25).margin(0.001));
+    CHECK(r[0] == Approx(0.25).margin(0.01));
+    CHECK(r[1] == Approx(0.25).margin(0.01));
+    CHECK(r[2] == Approx(0.25).margin(0.01));
+    CHECK(r[3] == Approx(0.25).margin(0.01));
   }
 }
 
@@ -52,17 +52,17 @@ TEST_CASE("Simulation, basics, larger", "[simulation]") {
     CHECK(r[7] == Approx(0.125).margin(0.1));
   }
 
-  SECTION("Eval,smaller margins") {
-    auto r   = t.eval(1000000);
+  SECTION("Eval, smaller margins") {
+    auto r   = t.eval(100000);
     auto sum = std::accumulate(r.begin(), r.end(), 0.0);
     CHECK(sum == Approx(1.0));
-    CHECK(r[0] == Approx(0.125).margin(0.001));
-    CHECK(r[1] == Approx(0.125).margin(0.001));
-    CHECK(r[2] == Approx(0.125).margin(0.001));
-    CHECK(r[3] == Approx(0.125).margin(0.001));
-    CHECK(r[4] == Approx(0.125).margin(0.001));
-    CHECK(r[5] == Approx(0.125).margin(0.001));
-    CHECK(r[6] == Approx(0.125).margin(0.001));
-    CHECK(r[7] == Approx(0.125).margin(0.001));
+    CHECK(r[0] == Approx(0.125).margin(0.01));
+    CHECK(r[1] == Approx(0.125).margin(0.01));
+    CHECK(r[2] == Approx(0.125).margin(0.01));
+    CHECK(r[3] == Approx(0.125).margin(0.01));
+    CHECK(r[4] == Approx(0.125).margin(0.01));
+    CHECK(r[5] == Approx(0.125).margin(0.01));
+    CHECK(r[6] == Approx(0.125).margin(0.01));
+    CHECK(r[7] == Approx(0.125).margin(0.01));
   }
 }
